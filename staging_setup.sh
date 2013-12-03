669f52a26e38e3534ef4ad147ef89c23b32a0584
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-03 14:44:54 +0100

    first commit

---
+++ staging_setup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#https://wiki.mozilla.org/ReleaseEngineering/How_To/Setup_Personal_Development_Master#Create_a_build_master
+set -e
+cd "$(dirname $0)"
+
+USERNAME="$(whoami)"
+ROLE="build"
+BASEDIR="/builds/buildbot/$USERNAME/staging" 
+
+if [ -e "$BASEDIR" ]
+then
+    echo "$BASEDIR already exists: terminating"
+    echo "backup and remove your $BASEDIR if you want to run this script"
+    exit 0
+fi
+
+
+TMP_DIR="$(mktemp -d XXXXXXXX)"
+
+cd "$TMP_DIR"
+#====================#
+# finding free ports #
+#====================#
+
+function http_port {
+    echo $((8000 + $1))
+}
+function ssh_port {
+    echo $((7000 +$1))
+}
+function pb_port {
+    echo $((9000 +$1))
+}
+
+#### beautiful solution provided by Pete ####
+function port_suffix {
+    while true; do
+        portsuffix=$[RANDOM % 1000]
+        SSH_PORT=$(ssh_port $portsuffix)
+        HTTP_PORT=$(http_port $portsuffix)
+        PB_PORT=$(pb_port $portsuffix)
+        if (! nc -z 127.0.0.1 $SSH_PORT || \
+            ! nc -z 127.0.0.1 $HTTP_PORT || \
+            ! nc -z 127.0.0.1 $PB_PORT)>/dev/null 
+        then 
+            echo $portsuffix
+            break
+        fi
+    done
+}
+#### end ####
+portsuffix=$(port_suffix)
+SSH_PORT=$(ssh_port $portsuffix)
+HTTP_PORT=$(http_port $portsuffix)
+PB_PORT=$(pb_port $portsuffix)
+
+#===============#
+# create master #
+#===============#
+hg clone http://hg.mozilla.org/build/buildbot-configs
+cd buildbot-configs
+
+make -f Makefile.setup \
+USE_DEV_MASTER=1 \
+MASTER_NAME="$USERNAME" \
+BASEDIR="$BASEDIR" \
+PYTHON=python2.6 \
+VIRTUALENV=virtualenv-2.6 \
+BUILDBOTCUSTOM_BRANCH=default \
+BUILDBOTCONFIGS_BRANCH=default \
+USER="$USERNAME" \
+HTTP_PORT="$HTTP_PORT" PB_PORT="$PB_PORT" SSH_PORT="$SSH_PORT" ROLE="$ROLE" \
+virtualenv deps install-buildbot master master-makefile
+
+rm -rf "$TMP_DIR"
