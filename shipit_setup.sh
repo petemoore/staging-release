b58655980aaea1ba50ccb2705e93753ef3fb6a7f
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-04 13:53:42 +0100

    new password generation; updated start up script for shipit

--- shipit_setup.sh
+++ shipit_setup.sh
@@ -11,7 +11,7 @@ ROLE="build"
 BASEDIR="/builds/buildbot/$USERNAME"
 SHIPIT_DIR="$BASEDIR/release-kickoff"
 SHIPIT_USER="shipit"
-PASSWORD="$(openssl rand -base64 32)"
+PASSWORD="$(openssl rand -base64 7 | sed 's/=//g' | sed 's!/!!g')"
 
 echo ""
 echo "Setting up a ship it for staging releases"
@@ -55,13 +55,13 @@ function port_suffix {
 
 port_suffix=$(port_suffix)
 SHIPIT_PORT="$(shipit_port $port_suffix)"
-cat <<'EOF' > "$SHIPIT_DIR/start_shipit.sh"
-#!/bin/bash
-echo "* http://dev-master01.build.scl1.mozilla.com:$SHIPIT_PORT/"
-echo "* user: $SHIPIT_USER"
-echo "* password: $PASSWORD"
-venv/bin/python kickoff-web.py -d sqlite:///kickoff.db -u "$SHIPIT_USER" -p "$PASSWORD" --host=0.0.0.0 --port="$SHIPIT_PORT"
-EOF
+echo "#!/bin/bash" > "$SHIPIT_DIR/start_shipit.sh"
+echo 'cd $(dirname$0)' >> "$SHIPIT_DIR/start_shipit.sh"
+echo "echo '* http://dev-master01.build.scl1.mozilla.com:$SHIPIT_PORT/'" >> "$SHIPIT_DIR/start_shipit.sh"
+echo "echo '* user: $SHIPIT_USER'" >> "$SHIPIT_DIR/start_shipit.sh"
+echo "echo '* password: $PASSWORD'" >> "$SHIPIT_DIR/start_shipit.sh"
+echo "$SHIPIT_DIR/venv/bin/python $SHIPIT_DIR/kickoff-web.py -d sqlite:///kickoff.db -u $SHIPIT_USER -p $PASSWORD --host=0.0.0.0 --port=$SHIPIT_PORT" >> "$SHIPIT_DIR/start_shipit.sh"
+
 chmod u+x "$SHIPIT_DIR/start_shipit.sh"
 echo "to start shipit, run: $SHIPIT_DIR/start_shipit.sh"
 echo "* http://dev-master01.build.scl1.mozilla.com:$SHIPIT_PORT/"
