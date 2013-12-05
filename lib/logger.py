c6678c1bd17a94f8d7c36f6c972b9e6e7e577234
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-05 21:04:35 +0100

    generic logger (uses logger.ini)

---
+++ lib/logger.py
@@ -0,0 +1,8 @@
+"""Log manager
+   import this module if you need to log
+"""
+import logging.config
+import os.path
+
+logging_conf = os.path.join(os.path.dirname(__file__), '..', 'logging.ini')
+logging.config.fileConfig(logging_conf)
