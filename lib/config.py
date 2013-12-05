c39eb1557aa73c3f537d0ae371011dd5b7dd49b9
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-05 21:04:07 +0100

    added logger and updated _set_runtime_values (lib/config.py)

--- lib/config.py
+++ lib/config.py
@@ -7,6 +7,10 @@ import pwd
 import ports
 import random
 import string
+import logger
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def get_username():
@@ -26,25 +30,55 @@ class ConfigError(Exception):
 
 
 class Config(ConfigParser.SafeConfigParser):
-    """
-    this class manages the configuration. It also populates the configuration
-    with somevalues (e.g. username) that can be determined only at run time.
+    """this class manages the configuration"""
+    def read_from(self, filenames):
+        """reads the configuration from a file or a list of files
+           and then generates some runtime specific values
+           as (ports, passwords,..)"""
+        self.read(filenames)
+        self._set_runtime_values()
 
-    """
-    def __init__(self):
-        ConfigParser.SafeConfigParser.__init__(self)
+    def _set_runtime_values(self):
+        """this method collects all the values that should be
+            determined at runtime"""
+        self._set_username()
+        self._set_shipit_password()
+        self._set_shipit_port()
+        self._set_master_ports()
+
+    def _set_username(self):
         # username cannot be hard coded in config.ini
         # it must be determined at run time
         # since username is a common option, let's have
         # it in the DEFAULT section so it will be available
         # in every section -- what about switching to python3 for
         # better interpolation?
-        self.set('DEFAULT', 'username', get_username())
+        """sets the username
+           assumption: your LDAP username is the same as your account
+           on devmaster-01
+        """
+        username = get_username()
+        log.debug('username: {0}'.format(username))
+        self.set('DEFAULT', 'username', username)
 
-    def shipit_password(self):
-        self.set('shipit', 'password', generate_random_password())
+    def _set_shipit_password(self):
+        """generates a random password for shipit"""
+        # so there is no need to store a password in config.ini
+        password = generate_random_password()
+        log.debug('shipit password: {0}'.format(password))
+        self.set('shipit', 'password', password)
 
-    def master_ports(self):
+    def _set_shipit_port(self):
+        """finds an empty port for shipit"""
+        shipit_base_port = int(self.get('shipit', 'shipit_port_range'))
+        available_ports = ports.available_in_range(shipit_base_port,
+                                                   shipit_base_port + 1000)
+        shipit_port = str(available_ports.pop())
+        log.debug('shipit port: {0}'.format(shipit_base_port))
+        self.set('shipit', 'port', shipit_port)
+
+    def _set_master_ports(self):
+        """finds three random ports for master (pb, ssh and http)"""
         # right now:
         # http port is in range 8000-8999
         # ssh port == http_port - 1000
@@ -66,9 +100,13 @@ class Config(ConfigParser.SafeConfigParser):
 
             if not ports.in_use(pb_port) and not ports.in_use(ssh_port):
                 # we have found 3 ports that fit into our algorithm!
-                self.set('master', 'ssh_port', ssh_port)
-                self.set('master', 'pb_port', pb_port)
-                self.set('master', 'http_port', http_port)
+                log.debug('master ports:')
+                log.debug('http: {0}'.format(http_port))
+                log.debug('ssh: {0}'.format(ssh_port))
+                log.debug('pb: {0}'.format(pb_port))
+                self.set('master', 'ssh_port', str(ssh_port))
+                self.set('master', 'pb_port', str(pb_port))
+                self.set('master', 'http_port', str(http_port))
                 return
 
         # no available ports!
