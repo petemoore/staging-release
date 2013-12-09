7a88f5fb71c3d2d0923ce128904acc5d43a15a74
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-09 12:59:01 +0100

    added argparse

--- staging_setup.py
+++ staging_setup.py
@@ -7,15 +7,22 @@ from lib.master import Master
 from lib.master import MasterError
 import lib.logger
 import logging
-
+import argparse
 
 if __name__ == '__main__':
 
     log = logging.getLogger('staging release')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--bug', help='bug tracking id', required=True)
+    args = parser.parse_args()
+
     # reading configuration
     config = Config()
     config_ini = os.path.join(os.path.dirname(__file__), "config.ini")
     config.read_from(config_ini)
+    config.set('DEFAULT', 'tracking_bug', args.bug)
+    log.info(config)
     master = Master(config)
     try:
         master.install()
