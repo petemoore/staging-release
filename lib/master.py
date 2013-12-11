872e74ad21a331028be999a57840e3999ffea862
Massimo Gervasini <mgervasini@mozilla.com>
2013-12-11 11:30:30 +0100

    moved make_cmd into config.ini

--- lib/master.py
+++ lib/master.py
@@ -20,12 +20,6 @@ class Master(object):
     """creates a buildbot master"""
     def __init__(self, configuration):
         self.configuration = configuration
-        self.username = configuration.get('DEFAULT', 'username')
-        self.basedir = configuration.get('master', 'basedir')
-        self.http_port = configuration.get('master', 'http_port')
-        self.ssh_port = configuration.get('master', 'ssh_port')
-        self.pb_port = configuration.get('master', 'pb_port')
-        self.role = configuration.get('master', 'role')
         self.buildbot_configs_repo = configuration.get('master',
                                                        'buildbot_configs_repo')
 
@@ -61,21 +55,9 @@ class Master(object):
     def _make(self, cwd):
         """calls make to create a buildbot master"""
         log.info('creating master in {0}'.format(self.basedir))
-        make_cmd = ['-f', 'Makefile.setup']
-        make_cmd += ['USE_DEV_MASTER=1']
-        make_cmd += ['MASTER_NAME={0}'.format(self.username)]
-        make_cmd += ['BASEDIR={0}'.format(self.basedir)]
-        make_cmd += ['PYTHON=python2.6']
-        make_cmd += ['VIRTUALENV=virtualenv-2.6']
-        make_cmd += ['BUILDBOTCUSTOM_BRANCH=default']
-        make_cmd += ['BUILDBOTCONFIGS_BRANCH=default']
-        make_cmd += ['USER={0}'.format(self.username)]
-        make_cmd += ['HTTP_PORT={0}'.format(self.http_port)]
-        make_cmd += ['PB_PORT={0}'.format(self.pb_port)]
-        make_cmd += ['SSH_PORT={0}'.format(self.ssh_port)]
-        make_cmd += ['ROLE={0}'.format(self.role)]
-        make_cmd += ['virtualenv', 'deps', 'install-buildbot']
-        make_cmd += ['master', 'master-makefile']
+        config = self.configuration
+        make_cmd = config.get('master', 'make_cmd').splitlines()
+        log.debug('make command: {0}'.format(make_cmd))
 
         for line in make(make_cmd, _cwd=cwd, _iter=True):
             log.debug(line.strip())
